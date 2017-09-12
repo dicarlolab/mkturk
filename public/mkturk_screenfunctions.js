c5f62970af68960045e10361f79bbd30c37cb138
mlee3142 <mil@mit.edu>
2017-09-12 18:21:13 -0400

    canvas stuff

--- public/mkturk_screenfunctions.js
+++ public/mkturk_screenfunctions.js
@@ -99,7 +99,7 @@ function displayTerminalScreen(){
 
 
 //================== LOAD STATUS DISPLAY ==================//
-function refreshCanvasSettings(EXPERIMENT_entry){
+function refreshScreenTSequenceSettings(EXPERIMENT_entry){
 	// TODO: cleanup CANVAS; separate canvas ID from sequence logic; 'tsequence' variables coded by length rather than absolute time
 
 	// Adjust length / toggle presence of gray screen between sample and test screens
@@ -246,142 +246,12 @@ function setupTapTracker(){
 }
 
 
-function setupCanvas(canvasobj){
-	// center in page
-
-	var windowHeight = window.innerHeight
-	  || document.documentElement.clientHeight
-	  || document.body.clientHeight;
-
-
-	var windowWidth = window.innerWidth
-	  || document.documentElement.clientWidth
-	  || document.body.clientWidth;
-
-    // Reference: https://www.w3schools.com/js/js_window.asp
-	console.log('windowWidth', windowWidth, 'windowHeight', windowHeight)
-
-	canvasobj.style.top=CANVAS.offsettop + "px";
-	canvasobj.style.left=CANVAS.offsetleft + "px";
-	canvasobj.width=windowWidth - CANVAS.offsetleft;
-	canvasobj.height=windowHeight - CANVAS.offsettop;
-	canvasobj.style.margin="0 auto";
-	canvasobj.style.display="block"; //visible
-
-	// canvasobj.addEventListener('touchstart', function(e){e.preventDefault()})
-	// canvasobj.addEventListener('touchmove', function(e){e.preventDefault()})
-	
-	//assign listeners
-	//canvasobj.addEventListener('touchstart',touchstart_listener,{capture: false,passive: false}); // handle touch & mouse behavior independently http://www.html5rocks.com/en/mobile/touchandmouse/
-	//canvasobj.addEventListener('touchmove',touchmove_listener,{passive: false}) // based on console suggestion: Consider marking event handler as 'passive' to make the page more responive. https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
-	//canvasobj.addEventListener('touchend',touchend_listener,{capture: false, passive:false});
-	//canvasobj.addEventListener('mousedown',touchstart_listener,{capture: false,passive: false}); // handle touch & mouse behavior independently http://www.html5rocks.com/en/mobile/touchandmouse/
-	//canvasobj.addEventListener('mousemove',touchmove_listener,{passive: false}) // based on console suggestion: Consider marking event handler as 'passive' to make the page more responive. https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
-	//canvasobj.addEventListener('mouseup',touchend_listener,{capture: false, passive:false});
-} 
-
-// Sync: Adjust canvas for the device pixel ratio & browser backing store size
-// from http://www.html5rocks.com/en/tutorials/canvas/hidpi/#disqus_thread
-function scaleCanvasforHiDPI(canvasobj){
-	var windowHeight = window.innerHeight
-	  || document.documentElement.clientHeight
-	  || document.body.clientHeight;
-
-
-	var windowWidth = window.innerWidth
-	  || document.documentElement.clientWidth
-	  || document.body.clientWidth;
-
-	if (DEVICE.DevicePixelRatio !== backingStoreRatio){
-		context=canvasobj.getContext("2d");
-		var oldWidth = canvasobj.width;
-		var oldHeight = canvasobj.height;
-		canvasobj.width = oldWidth/DEVICE.CanvasRatio;
-		canvasobj.height = oldHeight/DEVICE.CanvasRatio;
-		canvasobj.style.width = windowWidth - CANVAS.offsetleft + "px";
-		canvasobj.style.height = windowHeight - CANVAS.offsettop + "px";
-		canvasobj.style.margin="0 auto";
-		context.scale(1/DEVICE.CanvasRatio,1/DEVICE.CanvasRatio);
-	} 
-} 
 
 
 
 
-//================== IMAGE RENDERING ==================//
-
-function defineImageGrid(ngridpoints, source_image_width, source_image_height, gridscale){
-
-	var windowHeight = window.innerHeight
-	  || document.documentElement.clientHeight
-	  || document.body.clientHeight;
 
 
-	var windowWidth = window.innerWidth
-	  || document.documentElement.clientWidth
-	  || document.body.clientWidth;
-
-	  
-	var unitgrid_x =[]
-	var unitgrid_y =[]
-	var xgridcent =[] 
-	var ygridcent =[]
-
-	var cnt=0;
-	for (var i=1; i<=ngridpoints; i++){
-		for (var j=1; j<=ngridpoints; j++){
-			unitgrid_x[cnt]=i - 1/2;
-			unitgrid_y[cnt]=j - 1/2;
-			cnt++;
-		}
-	}
-	//center x & y grid within canvas
-	var canvas_center_x = (windowWidth - CANVAS.offsetleft)*DEVICE.CanvasRatio*DEVICE.DevicePixelRatio/2
-	var dx = canvas_center_x - DEVICE.CanvasRatio*ngridpoints/2*source_image_width*gridscale; //left side of grid
-	var canvas_center_y = (windowHeight - CANVAS.offsettop)*DEVICE.CanvasRatio*DEVICE.DevicePixelRatio/2
-	var dy = canvas_center_y - DEVICE.CanvasRatio*ngridpoints/2*source_image_height*gridscale; //top of grid
-	for (var i=0; i<=unitgrid_x.length-1; i++){
-		xgridcent[i]=Math.round(unitgrid_x[i]*source_image_width*gridscale*DEVICE.CanvasRatio + dx);
-		ygridcent[i]=Math.round(unitgrid_y[i]*source_image_height*gridscale*DEVICE.CanvasRatio + dy);
-	}
-	return [canvas_center_x, canvas_center_y, xgridcent, ygridcent]
-}
-
-
-
-async function renderImageOnCanvas(image, grid_index, scale, canvasobj){
-	var context=canvasobj.getContext('2d');
-
-	var xleft=NaN;
-	var ytop=NaN;
-	var xbound=[];
-	var ybound=[];
-
-	wd = image.width
-	ht = image.height
-	xleft = Math.round(DEVICE.XGridCenter[grid_index] - 0.5*wd*scale*DEVICE.CanvasRatio);
-	ytop = Math.round(DEVICE.YGridCenter[grid_index] - 0.5*ht*scale*DEVICE.CanvasRatio);
-	
-	context.drawImage(
-		image, // Image element
-		xleft, // dx: Canvas x-coordinate of image's top-left corner. 
-		ytop, // dy: Canvas y-coordinate of  image's top-left corner. 
-		image.width*scale*DEVICE.CanvasRatio, // dwidth. width of drawn image. 
-		image.height*scale*DEVICE.CanvasRatio); // dheight. height of drawn image.
-
-	// For drawing cropped regions of an image in the canvas, see alternate input argument structures,
-	// See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
-	
-	// Bounding boxes of images on canvas
-	xbound=[xleft, xleft+wd*scale*DEVICE.CanvasRatio];
-	ybound=[ytop, ytop+ht*scale*DEVICE.CanvasRatio];
-
-	xbound[0]=xbound[0]+CANVAS.offsetleft;
-	xbound[1]=xbound[1]+CANVAS.offsetleft;
-	ybound[0]=ybound[0]+CANVAS.offsettop;
-	ybound[1]=ybound[1]+CANVAS.offsettop;
-	return [xbound, ybound]
-}
 
 class ScreenDisplayer{
 
@@ -463,11 +333,31 @@ class ScreenDisplayer{
 		context.fill();
 	}
 	async bufferFixationScreenUsingDot(gridindex){
+		funcreturn = await drawGridDots()
+		return
+
+		var dot_pixelradius = 10
+		var color = "white"
+		var context=CANVAS.obj.touchfix.getContext('2d');
+		context.clearRect(0,0,window.innerWidth, window.innerHeight);
+
+		// Draw fixation dot
+		var rad = dot_pixelradius;
+		for (var i = 0; i < DEVICE.XGridCenter.length; i++){
+			var xcent = DEVICE.XGridCenter[i];
+			var ycent = DEVICE.YGridCenter[i];
+			context.beginPath();
+			context.arc(xcent,ycent,rad,0*Math.PI,2*Math.PI);
+			context.fillStyle=color; 
+			context.fill();
+		}
+		
+
 		var dot_pixelradius = CANVAS.FixationRadius
 		var color = "white"
 
 		var context=CANVAS.obj.touchfix.getContext('2d');
-		context.clearRect(0,0,CANVAS.obj.touchfix.width,CANVAS.obj.touchfix.height);
+		//context.clearRect(0,0,CANVAS.obj.touchfix.width,CANVAS.obj.touchfix.height);
 
 		// Draw fixation dot
 		var rad = dot_pixelradius;
@@ -486,6 +376,8 @@ class ScreenDisplayer{
 	}
 
 	async bufferFixationScreenUsingImage(image, gridindex){
+		
+		
 
 		// Gray out first 
 		var canvasobj = CANVAS.obj.touchfix
@@ -496,7 +388,10 @@ class ScreenDisplayer{
 		// Draw image 
 
 		// todo: make general 'buffer' functions without hardcoding canvas references
-		funcreturn = await renderImageOnCanvas(image, gridindex, SubjectSettings['SampleScale'], canvasobj)
+
+		funcreturn = await drawGridDots()
+		return
+		funcreturn = await renderImageOnCanvasLiterally(image, gridindex, canvasobj)
 		
 	
 		// Define (rectangular) boundaries of fixation
