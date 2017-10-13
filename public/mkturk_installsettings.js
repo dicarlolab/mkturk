1b9028870243bc3684e71a91e92e6715e913505d
Elizabeth Yoo <elizabeth.yoo17@gmail.com>
2017-10-13 15:14:38 -0400

    change redirect URI

--- public/mkturk_installsettings.js
+++ public/mkturk_installsettings.js
@@ -16,10 +16,10 @@
 
 // MKTURK
 //var DBX_CLIENT_ID = "2m9hmv7q45kwren"
-var DBX_REDIRECT_URI_ROOT = "https://dl.dropboxusercontent.com/spa/k79b8ph6lmcr30d/mkturk/public/"
 
 //Liz 
 var DBX_CLIENT_ID = "7niq4ezmfinnfgy"
+var DBX_REDIRECT_URI_ROOT = "https://elizabethyoo.gitlab.io/projectpage/mkturk/public/mkturk.html"
 //var DBX_REDIRECT_URI_ROOT = "https://dl.dropboxusercontent.com/spa/k79b8ph6lmcr30d/mkturk/public/"
 //var DBX_REDIRECT_URI_ROOT = "https://docs.google.com/a/columbia.edu/document/d/e/2PACX-1vTWHwOSa5Ii3zJurZEqOqGlGXXNr5JNjJFEC52GC6Cu4tTuo3ZuZSK13bD51O8Ihz7jB94ac7ZrdXy1/pub"
 
