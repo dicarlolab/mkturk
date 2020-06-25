a99cff6a047a73f35a036dfda93107c67ca7d5fa
younahjeon <hiyuna1993@gmail.com>
2020-06-24 20:08:28 -0400

    minor change for animating transparency

--- public/mkturk/mkturk_threejs.js
+++ public/mkturk/mkturk_threejs.js
@@ -15,7 +15,6 @@ async function initThreeJS(scenedata) {
     // renderer.toneMappingExposure = 10;   // set exposure to light
     renderer.outputEncoding = THREE.sRGBEncoding;
 
-
     renderer.autoClear = false;
     renderer.setPixelRatio(ENV.ThreeJSRenderRatio)
     document.body.append(renderer.domElement);
@@ -154,7 +153,12 @@ async function addToScene(taskscreen){
                     material.map = child.material.map
                     child.material = material;
                     child.material.needsUpdate = true;
-                }//IF "Base" mesh
+               }//IF "Base" mesh
+               if (child.name == "Eyeriris" || child.name == "Eyeliris"){
+               	child.renderOrder = 1
+               }
+               child.material.transparent = true
+
             } //IF child.material
         }) //object.traverse (material)
     
@@ -308,6 +312,7 @@ async function addToScene(taskscreen){
                                 if (meshpartnames[m] == "Base" 
                                     && typeof(IMAGES[taskscreen][classlabel].OBJECTS[obj].baseVertexInd) != "undefined"
                                     && IMAGES[taskscreen][classlabel].OBJECTS[obj].baseVertexInd != [] ){
+
                                     var objectOriginvert = objects.getObjectByName(meshpartnames[m]).geometry.attributes.position.array
                                     var objectOriginvertind = IMAGES[taskscreen][classlabel].OBJECTS[obj].baseVertexInd
 
@@ -318,21 +323,18 @@ async function addToScene(taskscreen){
                                     morphOriginvertind = morphOriginvertind.map(function(num){return [...[num*3-3,num*3-2,num*3-1]]}).flat()
                                     objectOriginvertind = objectOriginvertind.map(function(num){return [...[num*3-3,num*3-2,num*3-1]]}).flat()
                                     
-         
-                                    morphTargetvertind = morphTargetvertind.map(function(num){return [...[num*3-3,num*3-2,num*3-1]]}).flat() 
-                                    morphOriginvertind = morphOriginvertind.map(function(num){return [...[num*3-3,num*3-2,num*3-1]]}).flat()
-                                    objectOriginvertind = objectOriginvertind.map(function(num){return [...[num*3-3,num*3-2,num*3-1]]}).flat()
-                                     
                                     morphTargetvert = morphTargetvert.subset(math.index(morphTargetvertind))
                                     morphOriginvert = morphOriginvert.subset(math.index(morphOriginvertind))
 
                                     var objectOriginvertdelta = math.zeros(objectOriginvert.length)
                                     objectOriginvertdelta.subset(math.index(objectOriginvertind), math.subtract(morphTargetvert,morphOriginvert)._data)
                                     morphTargetdelta[meshpartnames[m]].position = objectOriginvertdelta
+
                                     //normals 
                                     var morphTargetnormal = math.matrix(Array.from(morphTarget.getObjectByName(meshpartnames[m]).geometry.attributes.normal.array))
                                     var morphOriginnormal = math.matrix(Array.from(morphOrigin.getObjectByName(meshpartnames[m]).geometry.attributes.normal.array))
                                     var objectOriginnormal = objects.getObjectByName(meshpartnames[m]).geometry.attributes.normal.array
+
                                     morphTargetnormal = morphTargetnormal.subset(math.index(morphTargetvertind))
                                     morphOriginnormal = morphOriginnormal.subset(math.index(morphOriginvertind))
 
@@ -650,7 +652,7 @@ function updateObjectSingleFrame(taskscreen,objects,objPosition,objRotation,objS
 //==== OPACITY
     objects.traverse(function(child){
      if (child.material != undefined){
-      child.material.opacity = objOpacity      
+     		child.material.opacity = objOpacity 
      }
     }) //object.traverse (material)
 
@@ -746,7 +748,7 @@ function toTHREEJSOffset(x,y,taskscreen){
 }//FUNCTION toTHREEJSOffset
 
 function createMovieSeq(taskscreen,offdurationMSpre,ondurationMS,offdurationMSpost,framerate){
-    var movie_tseq = range(0,ondurationMS,1000/framerate); //Off times for sample frame
+    var movie_tseq = range(0,ondurationMS,Math.round(1000/framerate)); //Off times for sample frame
 
     if (offdurationMSpost > 0){
         //Postpend blank frame which starts at last Sample Frame OFF time
@@ -773,7 +775,7 @@ function createMovieSeq(taskscreen,offdurationMSpre,ondurationMS,offdurationMSpo
 
 function interpParam(vec,type,durationMS,framerate){
     var dur = durationMS/1000 //convert to seconds
-    var tseq = range(0,dur,1/framerate)
+    var tseq = range(0,dur,1/Math.round(framerate))
     var vec_flattened = Array(tseq.length).fill("")
 
     if (type == "binary"){vec[vec.length] = 0}//pad to get correct # of segments for step variables
@@ -801,6 +803,10 @@ function interpParam(vec,type,durationMS,framerate){
 
     if (isNaN(vec_flattened[vec_flattened.length-1])){
         vec_flattened[vec_flattened.length-1] = vec[vec.length-1]
+    }
+
+    if (type == "binary"){
+        vec_flattened[vec_flattened.length-1] = vec[vec.length-2]
     }
     return vec_flattened
 }//FUNCTION interpParam
