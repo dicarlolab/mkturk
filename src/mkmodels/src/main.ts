936594615e8e8e2d12891faf60d8b834131da0fd
hectorcho <yc3135@columbia.edu>
2020-11-24 16:35:29 -0500

    main.ts

--- src/mkmodels/src/main.ts
+++ src/mkmodels/src/main.ts
@@ -1,5 +1,5 @@
 import * as tf from '@tensorflow/tfjs';
-import { mod } from '@tensorflow/tfjs';
+import { Imag, mod, Tensor, TensorContainer, TensorContainerArray, train } from '@tensorflow/tfjs';
 // import * as mobilenet from '@tensorflow-models/mobilenet';
 import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -24,62 +24,173 @@ const storage = firebase.storage();
 const storageRef = storage.ref();
 const tokenVarRef = storageRef.child('/mkturkfiles/imagebags/objectome/wrench/flarenut_spanner/Var6NoBkgdNoPos_Batch1')
 const imgHolder = document.querySelector('#img-holder') as HTMLDivElement;
-console.log('hello');
+// console.log('hello');
 
+// tokenVarRef.listAll().then(res => {
+//   res.items.forEach(itemRef => {
+//     itemRef.getDownloadURL().then(url => {
+//       let tmpImg = document.createElement('img');
+//       tmpImg.crossOrigin = 'anonymous';
+//       tmpImg.src = url;
+//       tmpImg.width = 224;
+//       tmpImg.height = 224;
+//     });
+//   })
+// })
 
-tokenVarRef.listAll().then(res => {
-  res.items.forEach(itemRef => {
-    itemRef.getDownloadURL().then(async url => {
-      let tmpImg = document.createElement('img');
-      tmpImg.crossOrigin = 'anonymous';
-      tmpImg.src = url;
-      tmpImg.width = 224;
-      tmpImg.height = 224;
-      tmpImg.onload = () => {
-        let img = tf.browser.fromPixels(tmpImg);
-        img.print();
-        img = img.reshape([1, 224, 224, 3]);
-        img = tf.cast(img, 'float32');
-        let features = model.execute(img) as tf.Tensor;
-        features.print();
-        features.dispose();
-      }
-      
-    });
+// img = img.reshape([1, 224, 224, 3]);
+//         img = tf.cast(img, 'float32');
+//         let features = model.execute(img) as tf.Tensor;
+
+let trainDataArr: string[] = [];
+let trainDataFeatureArr: TensorContainerArray = [];
+let testDataArr = [];
+
+async function dothis() {
+  // await tokenVarRef.listAll().then(async res => {
+  //   await res.items.forEach(async itemRef => {
+  //     await itemRef.getDownloadURL().then(async url => {
+  //       // console.log('hello');
+  //       trainDataArr.push(url);
+  //       let features = await generateFeatureTensor(url);
+  //       console.log(features);
+  //       trainDataFeatureArr.push(features);
+  //     });
+  //   });
+  // });
+
+  await tokenVarRef.listAll().then(async res => {
+    for (let itemRef of res.items) {
+      await itemRef.getDownloadURL().then(async url => {
+        trainDataFeatureArr.push(await generateFeatureTensor(url));
+      });
+    }
+    return 0;
   });
-}).catch(err => {
-  console.error(`Err: ${err}`);
-});
 
-let arr = ['google.com'];
-
-function* data() {
-  for (let i = 0; i < 100; i++) {
-    let imgHolder = document.createElement('img');
-    imgHolder.crossOrigin = 'anonymous';
-    imgHolder.src = arr[i];
-    function* stuff() {
-      yield imgHolder.decode()
-    }
-    stuff.next()
-    imgHolder.width = 224;
-    imgHolder.height = 224;
-    let img = tf.browser.fromPixels(imgHolder);
-    img = img.reshape([1, 224, 224, 3]);
-    img = tf.cast(img, 'float32');
-    let features = model.execute(img) as tf.Tensor;
-    yield features;
-    features.dispose();
-    }
+  await trainModel();
 }
 
-function* labels() {
-  for (let i = 0; i < 100; i++) {
-    yield tf.randomUniform([10]);
+
+function loadImage(url: string) {
+  // console.log('hello2');
+  return new Promise((res, rej) => {
+    let img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src = url;
+    img.width = 224;
+    img.height = 224;
+    img.onload = () => {
+      res(img);
+    }
+  });
+}
+
+// img = img.reshape([1, 224, 224, 3]);
+//         img = tf.cast(img, 'float32');
+//         let features = model.execute(img) as tf.Tensor;
+
+const generateFeatureTensor = async (imgUrl: string) => {
+  let image = await loadImage(imgUrl) as HTMLImageElement;
+  let tmp = tf.browser.fromPixels(image);
+  tmp = tmp.reshape([1, 224, 224, 3]);
+  tmp = tf.cast(tmp, 'float32');
+  return model.execute(tmp) as tf.Tensor;
+}
+
+
+function* dataGenerator() {
+  // for (let i = 0; i < trainDataFeatureArr.length; i++) {
+  //   console.log('hello');
+  //   yield trainDataFeatureArr[i];
+  // }
+  const numelem = trainDataFeatureArr.length;
+  let idx = 0;
+  while (idx < numelem) {
+    idx++;
+    yield trainDataFeatureArr[idx];
   }
 }
 
-const xs = tf.data.generator(data);
+function* labelGenerator() {
+  const numelem = trainDataFeatureArr.length;
+  let idx = 0;
+  while (idx < numelem) {
+    idx++;
+    yield idx % 2;
+  }
+}
+
+async function trainModel() {
+  console.log('hellohello');
+  const xs = tf.data.generator(dataGenerator);
+  const ys = tf.data.generator(labelGenerator);
+  const ds = tf.data.zip({xs, ys});
+  await xs.forEachAsync(e => console.log(e));
+}
+
+async function mainmain() {
+  await dothis();
+  await trainModel();
+}
+
+mainmain();
+
+
+
+// tokenVarRef.listAll().then(res => {
+//   res.items.forEach(itemRef => {
+//     itemRef.getDownloadURL().then(async url => {
+//       let tmpImg = document.createElement('img');
+//       tmpImg.crossOrigin = 'anonymous';
+//       tmpImg.src = url;
+//       tmpImg.width = 224;
+//       tmpImg.height = 224;
+//       tmpImg.onload = () => {
+//         let img = tf.browser.fromPixels(tmpImg);
+//         img.print();
+//         img = img.reshape([1, 224, 224, 3]);
+//         img = tf.cast(img, 'float32');
+//         let features = model.execute(img) as tf.Tensor;
+//         features.print();
+//         features.dispose();
+//       }
+      
+//     });
+//   });
+// }).catch(err => {
+//   console.error(`Err: ${err}`);
+// });
+
+// let arr = ['google.com'];
+
+// function* data() {
+//   for (let i = 0; i < 100; i++) {
+//     let imgHolder = document.createElement('img');
+//     imgHolder.crossOrigin = 'anonymous';
+//     imgHolder.src = arr[i];
+//     function* stuff() {
+//       yield imgHolder.decode()
+//     }
+//     stuff.next()
+//     imgHolder.width = 224;
+//     imgHolder.height = 224;
+//     let img = tf.browser.fromPixels(imgHolder);
+//     img = img.reshape([1, 224, 224, 3]);
+//     img = tf.cast(img, 'float32');
+//     let features = model.execute(img) as tf.Tensor;
+//     yield features;
+//     features.dispose();
+//     }
+// }
+
+// function* labels() {
+//   for (let i = 0; i < 100; i++) {
+//     yield tf.randomUniform([10]);
+//   }
+// }
+
+// const xs = tf.data.generator(data);
 
 // function* data() {
 //   tokenVarRef.listAll().then(res => {
