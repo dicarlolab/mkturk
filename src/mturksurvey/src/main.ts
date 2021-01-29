c0d353ee58ca23cebba81a3c8d7dbd622df2ede3
hectorcho <yc3135@columbia.edu>
2021-01-29 11:07:03 -0500

    survey form function

--- src/mturksurvey/src/main.ts
+++ src/mturksurvey/src/main.ts
@@ -17,131 +17,101 @@ firebase.initializeApp(firebaseConfig);
 
 const functions = firebase.functions();
 const auth = firebase.auth();
-// const storage = firebase.app().storage('gs://mkturk-mturk');
+const countriesList = [
+  "Afghanistan", "Albania", "Algeria", "American Samoa", "Angola", "Anguilla",
+  "Antartica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba",
+  "Ashmore and Cartier Island", "Australia", "Austria", "Azerbaijan", "Bahamas",
+  "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
+  "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil",
+  "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi",
+  "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic",
+  "Chad", "Chile", "China", "Christmas Island", "Clipperton Island", "Cocos (Keeling) Islands",
+  "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the",
+  "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czeck Republic",
+  "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador",
+  "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Europa Island",
+  "Falkland Islands (Islas Malvinas)", "Faroe Islands", "Fiji", "Finland", "France",
+  "French Guiana", "French Polynesia", "French Southern and Antarctic Lands", "Gabon",
+  "Gambia, The", "Gaza Strip", "Georgia", "Germany", "Ghana", "Gibraltar",
+  "Glorioso Islands", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala",
+  "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands",
+  "Honduras", "Hong Kong", "Howland Island", "Hungary", "Iceland",
+  "India", "Indonesia", "Iran", "Iraq", "Ireland", "Ireland, Northern", "Israel", "Italy",
+  "Jamaica", "Jan Mayen", "Japan", "Jarvis Island", "Jersey", "Johnston Atoll", "Jordan",
+  "Juan de Nova Island", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South",
+  "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
+  "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia",
+  "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Man, Isle of",
+  "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico",
+  "Micronesia, Federated States of", "Midway Islands", "Moldova", "Monaco", "Mongolia",
+  "Montserrat", "Morocco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands",
+  "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria",
+  "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau",
+  "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcaim Islands", "Poland",
+  "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romainia", "Russia", "Rwanda", "Saint Helena",
+  "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
+  "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe",
+  "Saudi Arabia", "Scotland", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia",
+  "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+  "South Georgia and South Sandwich Islands", "Spain", "Spratly Islands", "Sri Lanka", "Sudan",
+  "Suriname", "Svalbard", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan",
+  "Tanzania", "Thailand", "Tobago", "Toga", "Tokelau", "Tonga", "Trinidad", "Tunisia", "Turkey",
+  "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay",
+  "USA", "Uzbekistan", "Vatican City", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands", "Wales",
+  "Wallis and Futuna", "West Bank", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
+];
 
 let mturkUserConfig: any = {};
 console.log('location:', window.location.search);
+if (window.location.search) {
+  try {
+    let mturkCfgPairStr = window.location.search.split('?')[1].split('&');
+    mturkCfgPairStr.forEach(str => {
+      let pair = str.split('=');
+      if (pair[0] == 'AID') { // AID: assignmentId
+        mturkUserConfig.aid = pair[1];
+      } else if (pair[0] == 'HID') { // HID: hitId
+        mturkUserConfig.hid = pair[1];
+      } else if (pair[0] == 'WID') { // WID: workerId
+        mturkUserConfig.wid = pair[1];
+      } else if (pair[0] == 'TASK') { // TASK: name of task in params_storage
+        mturkUserConfig.task = pair[1];
+      }
+    });
+  } catch (e) {
+    console.error('Error Parsing User Config:', e);
+  }
+}
+console.log(mturkUserConfig);
 
 const surveyForm = document.querySelector('#survey-form') as HTMLFormElement;
 surveyForm.addEventListener('submit', (evt: Event) => {
-  console.log('hello');
+  // prevent submit form on ENTER/RETURN
+  evt.preventDefault();
+  // 1. Check that all required elements are populated
+  let difficultyChecked = document.querySelectorAll('input[name=difficulty]:checked').length;
+  let engagingChecked = document.querySelectorAll('input[name=engaging]:checked').length;
+  const requiredElems = (
+    document.querySelectorAll('.required') as NodeListOf<HTMLInputElement | HTMLSelectElement>
+  );
+  try {
+    if (!difficultyChecked) {
+      throw new TypeError(`DIFFICULTY level is not checked`);
+    }
+
+    if (!engagingChecked) {
+      throw new TypeError(`ENGAGING level is not checked`);
+    }
+    requiredElems.forEach((requiredEl: HTMLInputElement | HTMLSelectElement) => {
+      if (!requiredEl.value) {
+        throw new TypeError(`value of ${requiredEl.id} is undefined`);
+      }
+    });
+
+    console.log('all values look good');
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+  }
+  
 });
-
-const submitBtn = document.querySelector('#submit-btn') as HTMLInputElement;
-submitBtn.addEventListener('pointerup', (evt: Event) => {
-  console.log('hello');
-  let submitEvent = new Event('submit');
-  surveyForm.dispatchEvent(submitEvent);
-});
-
-// let workerId = Math.random().toString(36).substr(2);
-// console.log(workerId);
-
-// console.log(window);
-// try {
-//   let mturkCfgPairStr = window.location.search.split('?')[1].split('&');
-//   mturkCfgPairStr.forEach(str => {
-//   let pair = str.split('=');
-//   if (pair[0] == 'AID') { // AID: assignmentId
-//     mturkUserConfig.aid = pair[1];
-//   } else if (pair[0] == 'HID') { // HID: HITId
-//     mturkUserConfig.hid = pair[1];
-//   } else if (pair[0] == 'WID') { // WID: workerId
-//     mturkUserConfig.wid = pair[1];
-//   } else if (pair[0] == 'TASK') {
-//     mturkUserConfig.task = pair[1];
-//   }
-//   console.log('pair:', pair);
-//   });
-// } catch (e) {
-//   console.error('e:', e);
-// }
-
-
-// const isLabMember = functions.httpsCallable('isLabMember');
-// const isMturkUser = functions.httpsCallable('isMturkUser');
-// const decodeToken = functions.httpsCallable('decodeToken');
-// const processMturkUser = functions.httpsCallable('processMturkUser');
-// const copyParamFile = functions.httpsCallable('copyParamFile');
-// const submitAssignment = functions.httpsCallable('submitAssignment');
-
-// const signOutBtn = (
-//   document.querySelector('#sign-out-btn') as HTMLButtonElement
-// );
-
-// const doneBtn = (
-//   document.querySelector('#done-btn') as HTMLButtonElement
-// );
-
-// const submitCodeSpan = (
-//   document.querySelector('#submit-code-span') as HTMLSpanElement
-// );
-
-// doneBtn.addEventListener('click', async (ev: Event) => {
-//   ev.preventDefault();
-//   submitCodeSpan.textContent = 'Loading...';
-//   let submissionState = await submitAssignment(mturkUserConfig);
-//   submitCodeSpan.textContent = submissionState.data.message;
-//   console.log(submissionState);
-// });
-
-// signOutBtn.addEventListener('click', (ev: Event) => {
-//   ev.preventDefault();
-//   auth.signOut().then(() => {
-//     console.log('[SignOut]: Success');
-//   }).catch(error => {
-//     console.error('[SignOut] Error:', error);
-//   });
-// });
-
-
-// auth.getRedirectResult().then(redirectResult => {
-//   if (redirectResult.credential) {
-//     console.log('User Signed In Now');
-//     console.log('redirectResult.credential:', redirectResult.credential);
-//   } else if (auth.currentUser) {
-//     console.log('User Already Signed In');
-//     console.log('auth.currentUser:', auth.currentUser);
-//   } else {
-//     console.log('User Not Yet Authenticated');
-//     let provider = new firebase.auth.GoogleAuthProvider();
-//     provider.addScope('https://www.googleapis.com/auth/user.emails.read');
-//     provider.addScope('https://www.googleapis.com/auth/userinfo.email');
-//     auth.signInWithRedirect(provider);
-//   }
-// }).catch(e => {
-//   console.error('[Authentication Error]', e);
-// });
-
-// auth.onAuthStateChanged(user => {
-//   if (user) {
-//     user.getIdToken(true).then(async idToken => {
-//       mturkUserConfig.token = idToken;
-//       console.log('token:', idToken);
-//       processMturkUser(mturkUserConfig).then(async res => {
-//         console.log('[processMturkUser] Result:', res);
-//         if (res.data.message == 'assignment entry already exists') {
-//           console.log('same');
-//           window.close();
-//         }
-//       }).catch(error => {
-//         console.error('[processMturkUser] Error:', error);
-//       });
-//     });
-//   }
-// });
-
-
-// let res = await copyParamFile();
-// console.log(res);
-
-
-// let fileRef = storage.ref('mkturkfiles/scenebags/objectome3d/camel/hello.txt');
-
-// let fileUrl = await fileRef.getDownloadURL().catch(e => {
-//   console.error("Error getting download URL", e);
-// });
-
-// let response = await fetch(fileUrl);
-// // let file = await response.json();
-// console.log(await response.text());
