746cc737e48f0214529eadb5093dcee3a0c8c6f1
hectorcho <yc3135@columbia.edu>
2021-07-28 18:24:56 -0400

    liveplot fix

--- src/liveplot3/src/charts.ts
+++ src/liveplot3/src/charts.ts
@@ -1,6 +1,11 @@
 import _, { last, sample } from 'lodash';
 import { FileType, LiveplotDataType } from './types';
 import { Utils } from './utils';
+import * as Highcharts from 'highcharts';
+import Exporting from 'highcharts/modules/exporting';
+Exporting(Highcharts);
+// import * as Exporting from 'highcharts/modules/exporting';
+
 
 const colorMapJet = [
   '#00008F','#00009F','#0000AF','#0000BF',
@@ -124,6 +129,57 @@ export class Charts {
     this.realtimeDataTable = new google.visualization.DataTable();
     this.rtData = {};
 
+    Highcharts.chart({
+      title: {
+        text: 'HIGHCHART EXAMPLE'
+      },
+
+      plotOptions: {
+        series: {
+          label: {
+            connectorAllowed: false
+          },
+          pointStart: 2010
+        }
+      },
+      exporting: {
+        enabled: false
+      },
+      chart: {
+        renderTo: document.querySelector('#highchart-plot') as HTMLDivElement
+      },
+      
+
+      series: [{
+          name: 'Installation',
+          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+          type: 'line'
+        },
+        {
+          name: 'Manufacturing',
+          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+          type: 'line'
+        },
+        {
+          name: 'Sales & Distribution',
+          data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+          type: 'line'
+        },
+        {
+          name: 'Project Development',
+          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+          type: 'line'
+        },
+        {
+          name: 'Other',
+          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+          type: 'line'
+      }]
+
+      
+    });
+
+    
   }
 
   public async setupCharts() {
